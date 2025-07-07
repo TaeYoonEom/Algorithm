d05cd94ede7769d85e9a3e0d811a5f13e8256ed2
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2025-07-08 00:50:12 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 13.55 ms, Memory: 7.35 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.py
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.py
@@ -1,8 +1,12 @@
-s = input()
-for c in s:
-    if 'a' <= c <= 'z':
-        print(chr(ord(c) - 32), end='')
-    elif 'A' <= c <= 'Z':
-        print(chr(ord(c) + 32), end='')
+text = input()
+result = ''
+
+for ch in text:
+    if ch.islower():
+        result += ch.upper()
+    elif ch.isupper():
+        result += ch.lower()
     else:
-        print(c, end='')
+        result += ch  # 공백이나 숫자 같은 건 그대로 추가
+
+print(result)
