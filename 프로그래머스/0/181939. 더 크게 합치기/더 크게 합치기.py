aa7ef925daeb9927768ce537a0555820c7088027
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-02-12 20:18:07 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,10 +1,14 @@
 def solution(a, b):
     answer = 0
+    string1 = ''
+    string2 = ''
     
-    a = str(a)
-    b = str(b)
-    if(a + b > b + a):
-        answer = a + b
+    string1 = str(a) + str(b)
+    string2 = str(b) + str(a)
+    
+    if string1 > string2:
+        answer = int(string1)
     else:
-        answer = b + a
-    return int(answer)
+        answer = int(string2)
+    
+    return answer
