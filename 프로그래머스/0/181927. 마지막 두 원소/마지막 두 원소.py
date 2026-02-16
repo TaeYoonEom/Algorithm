6e2c1fc659d2ad8c246d526c947334982d69fa69
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-02-16 15:16:54 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -1,9 +1,8 @@
 def solution(num_list):
-    answer = []
+    answer = num_list
     
     if num_list[-1] > num_list[-2]:
-        num_list.append(num_list[-1] - num_list[-2])
-    else:
-        num_list.append(num_list[-1] * 2) 
-        
-    return num_list
+        answer.append(num_list[-1] - num_list[-2])
+    elif num_list[-1] <= num_list[-2]:
+        answer.append(num_list[-1]*2)
+    return answer
