38e7711614c6901b0a2a07e6b9358400facf3ac1
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-02-22 13:25:31 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 8.95 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/README.md
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 수열과 구간 쿼리 2 - 181923 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181923) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181923#qna) 
 
 ### 성능 요약
 
-메모리: 9.37 MB, 시간: 7.33 ms
+메모리: 9.39 MB, 시간: 8.95 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 19일 20:13:47
+2026년 02월 22일 13:25:29
 
 ### 문제 설명
 
--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -1,10 +1,17 @@
 def solution(arr, queries):
     answer = []
+    
     for s, e, k in queries:
-        candidates = [x for x in arr[s:e+1] if x > k]
+        sub = arr[s:e+1]
+        
+        candidates = []
+        for x in sub:
+            if x > k:
+                candidates.append(x)
         
         if candidates:
             answer.append(min(candidates))
         else:
             answer.append(-1)
+        
     return answer
