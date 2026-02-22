ebfc510ffce4fdcdb0530093b70d7134f39e03ec
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-02-22 13:34:16 +0900

    [level 0] Title: l로 만들기, Time: 8.44 ms, Memory: 9.57 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.py
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -2,9 +2,8 @@ def solution(myString):
     answer = ''
     
     for i in myString:
-        if i < 'l':
-            answer += 'l'
+        if i < "l":
+            answer += "l"
         else:
             answer += i
-    
     return answer
