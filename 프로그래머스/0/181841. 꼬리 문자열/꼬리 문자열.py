4897e3434e74c5fe98197fb596b3ae658685b760
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-02-23 16:28:22 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.00 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.py
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.py
@@ -1,8 +1,7 @@
 def solution(str_list, ex):
     answer = ''
     
-    for i in range(len(str_list)):
-        if ex not in str_list[i]:
-            answer += str_list[i]
-            
+    for i in str_list:
+        if ex not in i:
+            answer += i
     return answer
