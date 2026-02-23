2d0bed1aa8c6d976ba8212ce72b2a6f37c1a183b
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-02-23 17:09:36 +0900

    [level 0] Title: 배열 만들기 2, Time: 489.53 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,22 +1,12 @@
-from collections import deque
-
 def solution(l, r):
-    result = []
-    queue = deque(["5"])  # 0으로 시작하지 않도록 "5"부터 시작
-
-    while queue:
-        num_str = queue.popleft()
-        num = int(num_str)
-
-        if num > r:
-            continue  # r보다 크면 무시
-
-        if l <= num <= r:
-            result.append(num)
-
-        # 다음 단계 숫자 생성
-        queue.append(num_str + "0")
-        queue.append(num_str + "5")
-
-    # 결과가 없으면 -1 반환
-    return sorted(result) if result else [-1]
+    answer = []
+    
+    for i in range(l, r+1):
+        s = str(i)
+        if all(c in "05" for c in s):
+            answer.append(i)
+    
+    if not answer:
+        return [-1]
+    
+    return answer
