1ef716ddc4eeff23484c1c819f546142f99adda1
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-02-26 18:26:56 +0900

    [level 0] Title: 글자 지우기, Time: 0.02 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
@@ -1,5 +1,7 @@
 def solution(my_string, indices):
+    answer = ''
+    indices.sort(reverse=True)
     
-    for i in sorted(indices, reverse=True):
+    for i in indices:
         my_string = my_string[:i] + my_string[i+1:]
     return my_string
