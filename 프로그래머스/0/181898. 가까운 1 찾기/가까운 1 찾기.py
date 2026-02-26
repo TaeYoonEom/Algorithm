088901395e145c65bdecfb02b058f9811a1de620
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-02-26 18:57:20 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.01 ms, Memory: 9.93 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -1,5 +1,7 @@
 def solution(arr, idx):
-    for i in range(idx, len(arr)):
+    for i in range(idx, len(arr)):   # idx 포함
         if arr[i] == 1:
             return i
     return -1
+        
+    ## idx 보다 인덱스가 크면서 값이 1인 가장 작은 인덱스 
