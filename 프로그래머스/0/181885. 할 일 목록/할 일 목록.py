dc6857f9154fe96a3e29ec0a13464e9c5b630d0f
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-03-05 23:58:48 +0900

    [level 0] Title: 할 일 목록, Time: 0.02 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -1,7 +1,7 @@
 def solution(todo_list, finished):
     answer = []
     
-    for i in range(len(todo_list)):
+    for i in range(len(finished)):
         if finished[i] == False:
             answer.append(todo_list[i])
     return answer
