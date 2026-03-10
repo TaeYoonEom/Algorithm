a84d6d91aca710b13c16891bda2bda6a36f8aff1
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-03-10 16:32:32 +0900

    [level 0] Title: 배열 만들기 6, Time: 145.13 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -1,15 +1,14 @@
 def solution(arr):
     answer = []
-    i = 0
-    while i < len(arr):
-        if answer == []:
+    
+    for i in range(len(arr)):
+        if not answer:
             answer.append(arr[i])
-            i += 1
-        elif answer != [] and answer[-1] == arr[i]:
-            answer = answer[: -1]
-            i += 1
-        elif answer != [] and answer[-1] != arr[i]:
+        elif answer[-1] == arr[i]:
+            answer.pop()
+        elif answer[-1] != arr[i]:
             answer.append(arr[i])
-            i += 1
-            
-    return answer if answer else [-1]
+    
+    if not answer:
+        return [-1] 
+    return answer
