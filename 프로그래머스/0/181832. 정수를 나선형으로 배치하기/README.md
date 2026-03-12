9136188ce7f7be7de6d4a8e4abdc25c43f904dfe
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-03-13 02:11:53 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.23 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/README.md
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 정수를 나선형으로 배치하기 - 181832 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181832#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181832) 
 
 ### 성능 요약
 
-메모리: 9.22 MB, 시간: 0.47 ms
+메모리: 9.28 MB, 시간: 0.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 29일 19:32:04
+2026년 03월 13일 02:11:51
 
 ### 문제 설명
 
--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -1,28 +1,24 @@
 def solution(n):
-    # 1. n x n 배열 생성 (0으로 초기화)
-    board = [[0] * n for _ in range(n)]
     
-    # 2. 방향: 오른쪽, 아래, 왼쪽, 위
-    dx = [0, 1, 0, -1]  # 행 이동
-    dy = [1, 0, -1, 0]  # 열 이동
+    arr = [[0]*n for _ in range(n)]
     
-    x, y, direction = 0, 0, 0  # 시작 위치 및 방향
-    num = 1  # 채울 숫자
-
-    for _ in range(n * n):
-        board[x][y] = num
-        num += 1
+    dx = [0,1,0,-1]   # → ↓ ← ↑
+    dy = [1,0,-1,0]
+    
+    x,y = 0,0
+    direction = 0
+    
+    for num in range(1, n*n+1):
+        arr[x][y] = num
         
-        # 다음 위치 계산
-        nx, ny = x + dx[direction], y + dy[direction]
+        nx = x + dx[direction]
+        ny = y + dy[direction]
         
-        # 조건 확인: 범위 내 && 아직 안 채운 곳
-        if 0 <= nx < n and 0 <= ny < n and board[nx][ny] == 0:
-            x, y = nx, ny
-        else:
-            # 방향 전환
-            direction = (direction + 1) % 4
-            x += dx[direction]
-            y += dy[direction]
+        if nx<0 or ny<0 or nx>=n or ny>=n or arr[nx][ny] != 0:
+            direction = (direction+1) % 4
+            nx = x + dx[direction]
+            ny = y + dy[direction]
+            
+        x,y = nx,ny
     
-    return board
+    return arr
