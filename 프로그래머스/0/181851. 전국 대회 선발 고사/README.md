545bb43f167df2e2aab05cf28fc3bc637df4c83c
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-03-13 02:04:03 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.03 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/README.md
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/README.md
@@ -1,10 +1,10 @@
 # [level 0] 전국 대회 선발 고사 - 181851 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181851) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181851?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.02 MB, 시간: 0.03 ms
+메모리: 9.2 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 28일 18:22:58
+2026년 03월 13일 02:04:00
 
 ### 문제 설명
 
--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -1,12 +1,15 @@
 def solution(rank, attendance):
-    # 출석한 학생들의 인덱스만 필터링
-    candidates = [i for i in range(len(attendance)) if attendance[i]]
-
-    # 출석한 학생들 중에서 등수가 낮은 순서대로 정렬
-    top3 = sorted(candidates, key=lambda i: rank[i])[:3]
-
-    # 인덱스 a, b, c 추출
-    a, b, c = top3
-
-    # 계산
+    
+    students = []
+    
+    for i in range(len(rank)):
+        if attendance[i] == True:
+            students.append((rank[i], i))
+    
+    students.sort()
+    
+    a = students[0][1]
+    b = students[1][1]
+    c = students[2][1]
+    
     return 10000 * a + 100 * b + c
