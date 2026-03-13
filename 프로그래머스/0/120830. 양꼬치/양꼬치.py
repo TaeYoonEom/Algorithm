fc978c3876c71bd80d96a8d0a97972d8000e9498
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-03-13 16:32:13 +0900

    [level 0] Title: 양꼬치, Time: 0.00 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.py
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.py
@@ -1,11 +1,7 @@
 def solution(n, k):
     answer = 0
     
-    yang = 12000
-    drink = 2000
     service = n // 10
-    if service >= 1:
-        k = k - service
-        
-    answer = n * 12000 + drink * k
+    
+    answer = (n * 12000 + 2000 * k) - service * 2000
     return answer
