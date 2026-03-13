f7a2273f2c5ed985f7475e1c8375aec239154b7a
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-03-13 16:21:22 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 9.05 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,9 +1,10 @@
 import math
 
 def solution(numer1, denom1, numer2, denom2):
-    a = (numer1 * denom2) + (numer2 * denom1)  # 분자
-    b = denom1 * denom2  # 분모
-    c = math.gcd(a, b)  # 최대공약수
-    answer = [a // c, b // c] # 기약분수
-
-    return answer
+    
+    numer = numer1 * denom2 + numer2 * denom1
+    denom = denom1 * denom2
+    
+    gcd = math.gcd(numer, denom)
+    
+    return [numer // gcd, denom // gcd]
