c0d8b72e53f7e76a38d417858ff03dd79cf52c68
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-03-14 00:11:58 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
@@ -1,9 +1,6 @@
 def solution(array):
-    answer = 0
-    number = len(array)
-    num = (number // 2) 
-    array.sort()
-    answer = array[num]
     
-    return answer
-        
+    array.sort()
+    
+    mid = len(array) // 2
+    return array[mid]
