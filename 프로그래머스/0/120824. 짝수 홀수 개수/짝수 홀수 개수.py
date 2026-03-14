9cc0d02c979bef2c2e749d007eca656b17d8e19c
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-03-15 00:41:27 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,12 +1,10 @@
 def solution(num_list):
-    answer = []
-    left = 0
-    right = 0
+    a = 0
+    b = 0
     
     for i in range(len(num_list)):
         if num_list[i] % 2 == 0:
-            left += 1
+            a += 1
         else:
-            right += 1
-    answer += (left, right)
-    return answer
+            b += 1
+    return [a, b]
