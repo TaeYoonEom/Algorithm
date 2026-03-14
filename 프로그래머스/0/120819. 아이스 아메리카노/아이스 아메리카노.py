272b89d2c59c4cc45d81d4e936d890229b232b2f
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-03-15 00:30:30 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.00 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -1,11 +1,6 @@
 def solution(money):
-    answer = []
-    icecoffee = 5500
-    count = 0
     
-    while(money >= icecoffee):
-        money -= icecoffee
-        count += 1
-        
-    answer += (count, money)
-    return answer
+    coffee = money // 5500
+    moneys = money % 5500
+    
+    return [coffee, moneys]
