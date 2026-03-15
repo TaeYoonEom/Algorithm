c11d58e86f466530b2116fbfc824f9e379388620
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-03-16 00:30:14 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -1,10 +1,5 @@
 def solution(emergency):
-    answer = []
-    for i in range(len(emergency)):
-        # 응급도보다 더 높은 값이 몇 개인지 세어서 순위 계산
-        count = 1
-        for j in range(len(emergency)):
-            if emergency[j] > emergency[i]:
-                count += 1
-        answer.append(count)
-    return answer
+    
+    sorted_arr = sorted(emergency, reverse = True)
+    ranks = [sorted_arr.index(x) + 1 for x in emergency]
+    return ranks
