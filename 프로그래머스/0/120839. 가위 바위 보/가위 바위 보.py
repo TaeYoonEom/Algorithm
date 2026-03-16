527b3070f8d165b9e170857fbf5ceaf164739eca
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-03-16 23:43:12 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 9 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,11 +1,11 @@
 def solution(rsp):
     answer = ''
     
-    for i in range(len(rsp)):
-        if rsp[i] == '2':
+    for i in rsp:
+        if i == '2':
             answer += '0'
-        elif rsp[i] == '0':
+        elif i == '0':
             answer += '5'
-        else:
+        elif i == '5':
             answer += '2'
     return answer
