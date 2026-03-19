cda8062d007c7e3e56b1393b7285ce8cd8b437b2
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-03-20 00:46:01 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.03 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.py
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -1,9 +1,9 @@
 def solution(s):
     stack = []
-    for token in s.split():   # 문자열을 공백으로 쪼갬
-        if token == 'Z':
-            if stack:         # 직전 숫자가 있으면 취소
-                stack.pop()
+    
+    for x in s.split():
+        if x == 'Z':
+            stack.pop()
         else:
-            stack.append(int(token))  # 문자열 → 정수 변환
+            stack.append(int(x))
     return sum(stack)
