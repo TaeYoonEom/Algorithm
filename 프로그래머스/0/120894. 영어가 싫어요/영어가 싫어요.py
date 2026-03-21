ab4c84a474fe39c07919949d5267eae8192fd3b5
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-03-22 00:25:05 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,11 +1,13 @@
 def solution(numbers):
-    alpha = {
-        "zero": '0', "one": '1', "two": '2',
-        "three": '3', "four": '4', "five": '5',
-        "six": '6', "seven": '7', "eight": '8', "nine": '9'
+    answer = 0
+    
+    d = {
+        "zero":"0","one":"1","two":"2",
+        "three":"3","four":"4","five":"5",
+        "six":"6","seven":"7","eight":"8","nine":"9"
     }
     
-    for word, digit in alpha.items():
-        numbers = numbers.replace(word, digit)
-    
+    for key, value in d.items():
+        numbers = numbers.replace(key, value)
+
     return int(numbers)
