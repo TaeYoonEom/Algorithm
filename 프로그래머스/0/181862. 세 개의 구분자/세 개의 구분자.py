dada4ae49e1ee95b6ec2e7a2e054a15442d89412
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-04-23 20:54:04 +0900

    [level 0] Title: 세 개의 구분자, Time: 27.05 ms, Memory: 22.5 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
@@ -1,13 +1,6 @@
+import re
 def solution(myStr):
-    # 1. 모든 구분자를 공통 구분자 하나로 바꾸기
-    for ch in 'abc':
-        myStr = myStr.replace(ch, '|')
     
-    # 2. 나누기
-    parts = myStr.split('|')
-    
-    # 3. 빈 문자열 제거
-    result = [s for s in parts if s]
-    
-    # 4. 결과 없으면 ["EMPTY"]
-    return result if result else ["EMPTY"]
+    split_lists = re.split('[abc]', myStr)
+    answer = [s for s in split_lists if s]
+    return answer if answer else ['EMPTY']
