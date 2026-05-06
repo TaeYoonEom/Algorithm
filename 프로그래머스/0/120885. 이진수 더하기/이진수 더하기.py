9006024c897a629f7c5b0aa559e0297ed5216352
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-05-06 16:02:48 +0900

    [level 0] Title: 이진수 더하기, Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.py
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.py
@@ -1,9 +1,10 @@
 def solution(bin1, bin2):
-    answer = ''
     
-    n1 = int(bin1, 2)
-    n2 = int(bin2, 2)
+    num1 = int(bin1, 2)
+    num2 = int(bin2, 2)
     
-    answer = n1 + n2 
+    total = num1 + num2
     
-    return bin(answer)[2:]
+    answer = bin(total)[2:]
+    
+    return answer
