842ef97b835e569b0bc99001617c64b1dc761ba9
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-05-10 13:41:46 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.01 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/README.md
+++ 프로그래머스/0/120861. 캐릭터의 좌표/README.md
@@ -1,10 +1,10 @@
 # [level 0] 캐릭터의 좌표 - 120861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120861#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120861) 
 
 ### 성능 요약
 
-메모리: 9.02 MB, 시간: 0.02 ms
+메모리: 9.1 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 10일 18:13:45
+2026년 05월 10일 13:41:44
 
 ### 문제 설명
 
--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -1,16 +1,21 @@
 def solution(keyinput, board):
     answer = [0, 0]
+    maxsize1 = board[0] // 2
+    maxsize2 = board[1] // 2
     
-    x = board[0] // 2
-    y = board[1] // 2
-    
-    for i in keyinput:
-        if i == "left" and answer[0] -1 >= -x:
-            answer[0] -= 1
-        elif i == "right" and answer[0] + 1 <= x:
-            answer[0] += 1
-        elif i == "down" and answer[1] - 1  >= -y:
-            answer[1] -= 1
-        elif i == "up" and answer[1] + 1 <= y:
-            answer[1] += 1
+    for i in range(len(keyinput)):
+        if keyinput[i] == 'left':
+            if -maxsize1 < answer[0]:
+                answer[0] -= 1
+        elif keyinput[i] == 'right':
+            if maxsize1 > answer[0]:
+                answer[0] += 1
+            
+        if keyinput[i] == 'up':
+            if maxsize2 > answer[1]:
+                answer[1] += 1
+        elif keyinput[i] == 'down':
+            if -maxsize2 < answer[1]:
+                answer[1] -= 1
+            
     return answer
