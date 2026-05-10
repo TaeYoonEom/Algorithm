644c38c3ce62b71153e6be17bea3921d55bc18e5
TaeYoonEom <147592079+TaeYoonEom@users.noreply.github.com>
2026-05-10 13:47:07 +0900

    [level 0] Title: OX퀴즈, Time: 0.08 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
@@ -1,12 +1,9 @@
 def solution(quiz):
     answer = []
     
-    for q in quiz:
-        # "3 - 4 = -1" -> ["3 - 4", "-1"]
-        left, right = q.split("=")
-        
-        # 왼쪽 부분 계산
-        if eval(left) == int(right):
+    for i in quiz:
+        parts = i.split('=')
+        if eval(parts[0]) == int(parts[1]):
             answer.append("O")
         else:
             answer.append("X")
--- 프로그래머스/0/120907. OX퀴즈/README.md
+++ 프로그래머스/0/120907. OX퀴즈/README.md
@@ -1,10 +1,10 @@
 # [level 0] OX퀴즈 - 120907 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120907#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120907) 
 
 ### 성능 요약
 
-메모리: 9.41 MB, 시간: 0.12 ms
+메모리: 9.22 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 05일 18:34:10
+2026년 05월 10일 13:47:04
 
 ### 문제 설명
 
